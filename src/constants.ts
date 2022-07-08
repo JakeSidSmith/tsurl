eb89e0d18a6117377914c7086e55a0c98be8c619
Jake 'Sid' Smith <hello@jakesidsmith.com>
2022-07-08 18:57:09 +0100

    Omit queryParams from default options

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,10 @@ export const MATCHES_MULTIPLE_SLASHES = /\/{2,}/g;
 export const MATCHES_MAYBE_TRAILING_SLASH = /\/?$/;
 export const MATCHES_MAYBE_PROTOCOL = /^(?:(?:[^/]+?:)?\/{0,2})?/;
 
-export const DEFAULT_OPTIONS: TSURLOptions = {
+export const DEFAULT_OPTIONS: Omit<
+  TSURLOptions<readonly never[]>,
+  'queryParams'
+> = {
   encode: true,
   decode: true,
   normalize: true,
